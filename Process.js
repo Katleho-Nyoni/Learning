08515154dff17723118325bbf0fff9b7eb9e22f6
Katieum <eaglenyoni@gmail.com>
2025-11-09 20:29:08 +0200

    File creation

---
+++ Process.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Katleho Nyoni
+ * Topic: Using the process module
+ *
+ * Can read environment variables, exit the process, let us interact with the terminal and get information about the current process.
+ * We can also collect info from the terminal like user input.
+ */
+
+console.log(process.argv);
+
+console.log("Process ID:", process.pid);
