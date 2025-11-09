fe8ed9fb6823784f57e6302dc9b165a97998b168
Katieum <eaglenyoni@gmail.com>
2025-11-09 20:02:24 +0200

    Getting current directory & filename

---
+++ root/Node-JS/Global.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Katleho Nyoni
+ * Topic: Global Objects & Scopes in Node.js
+ */
+console.log(__dirname);
+console.log(__filename);
+
+let dir = __dirname;
+let file = __filename;
+console.log(`Directory: ${dir}`);
+console.log(`File: ${file}`);
