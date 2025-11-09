f704330e75a557b8e6385c59de7f8a8fabbd5eec
Katieum <eaglenyoni@gmail.com>
2025-11-09 20:46:17 +0200

    Learning the basics

--- root/Node-JS/Process.js
+++ root/Node-JS/Process.js
@@ -6,6 +6,22 @@
  * We can also collect info from the terminal like user input.
  */
 
-console.log(process.argv);
+console.log(process.argv); //outputs location of node & this file
 
-console.log("Process ID:", process.pid);
+console.log("Process ID:", process.pid); // It changes each time
+
+/* Using the terminal
+paste: node Process --name Katleho --greeting "Hello, World!"
+*/
+
+function grab(flag) {
+  let indexAfterFlag = process.argv.indexOf(flag) + 1;
+  return process.argv[indexAfterFlag];
+}
+
+let name = grab("--name");
+let greeting = grab("--greeting");
+
+console.log(`${greeting}, from ${name}!`);
+
+process.stdout.write("Hello! "); // seems it wants to be imported first
