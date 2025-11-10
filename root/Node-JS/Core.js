8f2944cb2139a3b9e6d854fd8bca8b7f9a778f0d
Katieum <eaglenyoni@gmail.com>
2025-11-11 00:06:45 +0200

    Using heap-stats and filename

--- root/Node-JS/Core.js
+++ root/Node-JS/Core.js
@@ -1,8 +1,13 @@
 // import path from "path";
 const path = require("path");
 // const util = require("util");
+const v8 = require("v8");
 
 const dirUploads = path.join(__dirname, "src", "files", "uploads");
 
 console.log(dirUploads);
 // util.log(path.basename(__filename));
+console.log(path.basename(__filename));
+// util.log(v8.getHeapStatistics());
+
+console.log(v8.getHeapStatistics());
