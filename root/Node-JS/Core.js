4e5e2b0e7408a9981d910a70e0bbc52abaa77641
Katieum <eaglenyoni@gmail.com>
2025-11-11 00:03:44 +0200

    Maybe it needs to be downloaded first to work

--- root/Node-JS/Core.js
+++ root/Node-JS/Core.js
@@ -1,6 +1,8 @@
 // import path from "path";
 const path = require("path");
+// const util = require("util");
 
 const dirUploads = path.join(__dirname, "src", "files", "uploads");
 
 console.log(dirUploads);
+// util.log(path.basename(__filename));
