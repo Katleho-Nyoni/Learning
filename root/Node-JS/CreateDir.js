5cdb918485e2e174c56638b9b4817471665fa9f2
Katieum <eaglenyoni@gmail.com>
2025-11-13 01:08:01 +0200

    Create & move it

--- root/Node-JS/CreateDir.js
+++ root/Node-JS/CreateDir.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+/*
 fs.mkdir("NewDirName", (err) => {
   if (err) {
     console.log(`ERROR: ${err}`);
@@ -11,3 +12,15 @@ fs.mkdir("NewDirName", (err) => {
 fs.renameSync("./NewDirName/Sample.md", "./NewDirName/RenamedSample.md");
 
 console.log("File renamed successfully.");
+
+*/
+
+fs.mkdir("SampleDir", (err) => {
+  if (err) {
+    console.log(`ERROR: ${err}`);
+  } else {
+    console.log("Directory created successfully");
+  }
+});
+
+fs.renameSync("./SampleDir", "./NewDirName/SampleDir");
