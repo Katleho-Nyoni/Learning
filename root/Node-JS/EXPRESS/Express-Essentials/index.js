f21da0b1b957a996107bec09ef3dcfa0e5346c38
Katieum <eaglenyoni@gmail.com>
2025-11-14 01:28:40 +0200

    Mock-up API

--- root/Node-JS/EXPRESS/Express-Essentials/index.js
+++ root/Node-JS/EXPRESS/Express-Essentials/index.js
@@ -5,6 +5,22 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req,res) => {
+    res.send("This is a GET method route");
+})
+
+app.post('/api/establish', (req,res) => {
+    res.send("This is a POST method route");
+})
+
+app.put('/api/edit', (req,res) => {
+    res.send("This is a PUT method route");
+})
+
+app.delete('/api/delete', (req,res) => {
+    res.send("This is a DELETE method route");
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(data);
