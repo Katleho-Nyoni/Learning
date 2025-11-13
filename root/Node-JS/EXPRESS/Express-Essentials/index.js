b2a249b424f15daa5de45ba39b494ddff0d553cd
Katieum <eaglenyoni@gmail.com>
2025-11-13 23:57:31 +0200

    Mockup server

--- root/Node-JS/EXPRESS/Express-Essentials/index.js
+++ root/Node-JS/EXPRESS/Express-Essentials/index.js
@@ -1,1 +1,9 @@
-import 
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
