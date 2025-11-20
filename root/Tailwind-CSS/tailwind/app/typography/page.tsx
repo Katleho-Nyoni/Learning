839e880dd5ed2ea277066c9991a54acb72ff9cd0
Katieum <eaglenyoni@gmail.com>
2025-11-20 14:55:13 +0200

    Woo!

---
+++ root/Tailwind-CSS/tailwind/app/typography/page.tsx
@@ -0,0 +1,16 @@
+export default function Testing() {
+  return (
+    <>
+      <h1>Hello, World!</h1> <hr />
+      <div>
+        <p>
+          <span>&ldquo;</span>Using this tool has simplified our workflow and
+          boosted productivity. It's incredibly intuitive, and we've seen
+          noticeable improvements in efficiency across our entire team.
+        </p>
+        <p>Lea Burgot</p>
+        <p>Team Lead</p>
+      </div>
+    </>
+  );
+}
