8cd359ac8aff4e6df6fd7582345b67c5fb93bbb9
Katieum <eaglenyoni@gmail.com>
2025-11-21 11:26:24 +0200

    mockup

--- root/Tailwind-CSS/tailwind/app/typography/page.tsx
+++ root/Tailwind-CSS/tailwind/app/typography/page.tsx
@@ -3,14 +3,29 @@ export default function Testing() {
     <>
       <h1>Hello, World!</h1> <hr />
       <div>
+        <br />
         <p>
-          <span>&ldquo;</span>Using this tool has simplified our workflow and
-          boosted productivity. It's incredibly intuitive, and we've seen
-          noticeable improvements in efficiency across our entire team.
+          Using this tool has simplified our workflow and boosted productivity.
+          It`s incredibly intuitive, and we`ve seen noticeable improvements in
+          efficiency across our entire team.
         </p>
         <p>Lea Burgot</p>
         <p>Team Lead</p>
       </div>
+      <hr />
+      <section>
+        <h1>Welcome to Tailwind CSS</h1>
+        <span>
+          Rapidly build modern websites without ever leaving your HTML.
+        </span>
+        <p>
+          Tailwind CSS is a utility-first CSS framework packed with classes like
+          <code>flex</code>, <code>pt-4</code>, <code>text-center</code> and
+          <code>rotate-90</code>
+          that can be composed to build any design, directly in your markup.
+        </p>
+        <a href="https://www.tailwindcss.com">Learn more</a>
+      </section>
     </>
   );
 }
