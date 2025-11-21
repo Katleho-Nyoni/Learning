7faa2c5244a8d8b0d989e3538e4bd8cbd51a9abb
Katieum <eaglenyoni@gmail.com>
2025-11-21 22:36:26 +0200

    simple border

--- root/Tailwind-CSS/tailwind/app/typography/page.tsx
+++ root/Tailwind-CSS/tailwind/app/typography/page.tsx
@@ -13,28 +13,31 @@ export default function Testing() {
         <p>Team Lead</p>
       </div>
       <hr />
-      <section className="p-8 bg-linear-to-br from-blue-500 to-red-700">
-        <h1 className="font-bold text-4xl/12 text-blue-600">
-          Welcome to Tailwind CSS
-        </h1>
-        <span className="font-medium text-lg bg-amber-100 text-black">
-          Rapidly build modern websites without ever leaving your HTML.
-        </span>
-        <p className="text-sm">
-          Tailwind CSS is a utility-first CSS framework packed with classes like
-          <code className="font-bold">flex</code>,
-          <code className="font-bold">pt-4</code>,
-          <code className="font-bold">text-center</code> and
-          <code className="font-bold">rotate-90</code>
-          that can be composed to build any design, directly in your markup.
-        </p>
-        <a
-          className="underline underline-offset-4"
-          href="https://www.tailwindcss.com"
-        >
-          Learn more
-        </a>
-      </section>
+      <div className="p-8">
+        <section className="p-8 border-2 border-indigo-500">
+          <h1 className="font-bold text-4xl/12 text-blue-600">
+            Welcome to Tailwind CSS
+          </h1>
+          <span className="font-medium text-lg bg-amber-100 text-black">
+            Rapidly build modern websites without ever leaving your HTML.
+          </span>
+          <p className="text-sm">
+            Tailwind CSS is a utility-first CSS framework packed with classes
+            like
+            <code className="font-bold">flex</code>,
+            <code className="font-bold">pt-4</code>,
+            <code className="font-bold">text-center</code> and
+            <code className="font-bold">rotate-90</code>
+            that can be composed to build any design, directly in your markup.
+          </p>
+          <a
+            className="underline underline-offset-4"
+            href="https://www.tailwindcss.com"
+          >
+            Learn more
+          </a>
+        </section>
+      </div>
     </>
   );
 }
