655252137e6d80129ecf7b8bad974c528663084d
Katieum <eaglenyoni@gmail.com>
2025-11-22 16:05:14 +0200

    Adding height and width

--- root/Tailwind-CSS/tailwind/app/cards/page.tsx
+++ root/Tailwind-CSS/tailwind/app/cards/page.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import avatar from "./avatar.png";
 
 export default function Testing() {
   return (
     <>
       <h1>Hello, World!</h1>
       <hr />
-      <div>Card</div>
+      <div className="bg-indigo-500 text-white w-75 h-30">Card</div>
+      <hr />
+      <div>
+        <Image src={avatar} alt="" width={100} height={100} />
+        <p>Alin Fallow</p>
+        <p>Sent you a connection request</p>
+        <div>
+          <button>Accept</button>
+          <button>Decline</button>
+        </div>
+      </div>
+      <hr />
       <h1>My Blog</h1>
       <div>
         {/* <!-- Blog Post 1 --> */}
         <div>
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -22,7 +34,7 @@ export default function Testing() {
               stroke-linejoin="round"
               d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
             />
-          </svg>
+          </svg> */}
 
           <h2>How to Master Tailwind CSS</h2>
           <p>
@@ -84,18 +96,6 @@ export default function Testing() {
 
         <a href="#">Follow us on LinkedIn</a>
       </div>
-      <hr />
-
-      <div>
-        <Image src="images/avatar.png" alt="" />
-        <p>Alin Fallow</p>
-        <p>Sent you a connection request</p>
-        <div>
-          <button>Accept</button>
-          <button>Decline</button>
-        </div>
-      </div>
-      <hr />
     </>
   );
 }
