28c16f6fc4e09d4e84045a1e79ba532afb33746c
Katieum <eaglenyoni@gmail.com>
2025-11-23 02:00:29 +0200

    flex item & justify

--- root/Tailwind-CSS/tailwind/app/cards/page.tsx
+++ root/Tailwind-CSS/tailwind/app/cards/page.tsx
@@ -27,7 +27,7 @@ export default function Testing() {
       </div>
       <hr />
       <h1 className="font-bold text-3xl text-center">Services</h1>
-      <div>
+      <div className="mt-6 flex justify-evenly">
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
