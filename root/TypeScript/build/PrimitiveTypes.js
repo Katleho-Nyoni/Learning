bc985c75cccb43d9bbecaa8e3fcdb044ad65edfe
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-19 14:58:10 +0200

    Looking at the basics of TS

---
+++ root/TypeScript/build/PrimitiveTypes.js
@@ -0,0 +1,12 @@
+// JavaScript Primitive Types in TypeScript
+let a = 89; // number
+// TypeScript 
+let b = 56; // number
+let l;
+let m;
+let n;
+let u; // any type
+// let n = "Hello,World!" as any;
+console.log(a);
+console.log(b);
+console.log(n);
---
+++ root/TypeScript/src/PrimitiveTypes.ts
@@ -0,0 +1,16 @@
+// JavaScript Primitive Types in TypeScript
+
+let a = 89; // number
+
+// TypeScript 
+
+let b: number = 56; // number
+let l: string
+let m: string[]
+let n: boolean
+let u: any // any type
+
+// let n = "Hello,World!" as any;
+console.log(a);
+console.log(b);
+console.log(n);
