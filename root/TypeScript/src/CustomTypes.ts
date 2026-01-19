f9e487ebde6bfa8b91f5ecefc251233a3a60b157
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-19 15:02:15 +0200

    If a field is optional you use " ? "

--- root/TypeScript/src/CustomTypes.ts
+++ root/TypeScript/src/CustomTypes.ts
@@ -2,12 +2,11 @@ interface Constact {
     ID: number;
     name: string;
     email: string;
-    birthDate: Date;
+    birthDate?: Date;
 }
 
 let contact1: Constact = {
     ID: 1,
     name: "John Doe",
-    email: "johndoe@email.com",
-    birthDate: new Date("1990-01-01")
+    email: "johndoe@email.com"
 }
