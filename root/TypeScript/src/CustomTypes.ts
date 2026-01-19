4833ca9a9b6493e73e15be3f54513550bed50268
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-19 15:10:10 +0200

    Using type alias

--- root/TypeScript/src/CustomTypes.ts
+++ root/TypeScript/src/CustomTypes.ts
@@ -1,12 +1,24 @@
-interface Constact {
+interface Contact {
     ID: number;
     name: string;
     email: string;
     birthDate?: Date;
 }
 
-let contact1: Constact = {
+interface Employee extends Contact {
+    position: string;
+    salary: number;
+}
+
+let contact1: Contact = {
     ID: 1,
     name: "John Doe",
     email: "johndoe@email.com"
 }
+
+interface Gender {
+    Sex: boolean;
+    Chromosomes: LastPair;
+}
+
+type LastPair = "XX" | "XY"; // or = string
